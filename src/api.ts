eb05b340396d5643453e92b701ea8e6e527cade4
Siebe Baree <siebe.baree@outlook.com>
2022-12-31 16:14:53 +0100

    Added new middleware

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,9 @@ import routes from './routes';
 const app = express();
 
 app.use(cors({ origin: true }));
+app.use(express.json({
+    verify: (req, res, buf) => req['rawBody'] = buf
+}));
 app.use(express.json());
 app.use("/api", routes);
 
