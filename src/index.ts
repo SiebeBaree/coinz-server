b7676d5ef9e86abd14960f81d66f2f25e5591476
Siebe Baree <siebe.baree@outlook.com>
2022-12-31 16:14:59 +0100

    Added stats

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-// Environment Variables (Stripe API Key)
-import { config } from "dotenv"
-if (process.env.NODE_ENV !== 'production') {
-    config();
-}
-
+// Environment Variables
+import dotenv from "dotenv";
+dotenv.config();
 
 // Connect to MongoDB
 import { connect } from 'mongoose'
@@ -18,17 +15,63 @@ connect(process.env.DATABASE_URI, {
 }).then(() => console.log('Connected to MongoDB'));
 
 // Initialize Stripe
-// import Stripe from 'stripe';
-// export const stripe = new Stripe(process.env.STRIPE_SECRET, {
-//     apiVersion: '2022-08-01',
-// });
+import './stripe';
 
 // Start the API with Express
 import app from './api';
+import { createServer } from 'https';
+import { readFileSync } from 'fs';
+
+// Start the Stats Server
+import Stats from "sharding-stats";
+import { Request, Response } from "express";
+import { Shard } from "./utils/types";
+
+const StatsServer = new Stats.Server(app, {
+    selfHost: true,
+    stats_uri: `https://api.coinzbot.xyz/`, //Base URL 
+    authorizationkey: process.env.API_SHARDINGSTATS,
+    bot: { name: "s", icon: "s", website: "s", client_id: "s", client_secret: "s" }, redirect_uri: "s",
+    owners: ["643072638075273248"],
+});
+
+app.get('/api/status', (req: Request, res: Response) => {
+    const data = StatsServer.getStatsData().raw.shards;
+    const sorted = StatsServer.chunkShardsToClusterArrays(data);
+
+    const resData = [];
+    for (const { cluster, shards } of sorted) {
+        resData.push({
+            id: cluster,
+            shards: shards.map((shard: Shard) => ({
+                id: shard.id,
+                ping: shard.ping,
+                guildcount: shard.guildcount,
+                cpu: shard.cpu,
+                ram: shard.ram
+            })),
+        });
+    }
+
+    res.setHeader('Content-type', 'application/json');
+    res.send(resData);
+});
+
+StatsServer.on('error', console.error);
 
 const port = process.env.PORT || 3300;
-app.listen(port, () => console.log(`API available on http://localhost:${port}`));
+if (process.env.NODE_ENV === 'production') {
+    const options = {
+        key: readFileSync('/etc/ssl/coinzbot.xyz.key.pem'),
+        cert: readFileSync('/etc/ssl/coinzbot.xyz.cert.pem')
+    };
 
+    createServer(options, app).listen(port, () => console.log(`Server is running using https on port ${port}`));
+} else {
+    app.listen(port, () => console.log(`Server is running using http on port ${port}`));
+}
+
+// Handle uncaught exceptions and unhandled rejections
 const ignoredErrors = [];
 process.on('uncaughtException', (err: Error) => {
     if (!ignoredErrors.includes(`${err.name}: ${err.message}`)) {
