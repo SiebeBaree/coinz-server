92c506dda6b1518da0d542281b2925b735011e99
Siebe Baree <siebe.baree@outlook.com>
2022-12-31 16:15:58 +0100

    Added new base routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import discordRoutes from "./discord";
+import stripeRoutes from "./stripe";
+import storeRoutes from "./store";
 
 const router = Router();
 
 router.use("/discord", discordRoutes);
+router.use("/stripe", stripeRoutes);
+router.use("/store", storeRoutes);
 
 export default router;
