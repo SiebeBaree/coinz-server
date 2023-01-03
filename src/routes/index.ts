457d6e247db6f9fa30f5c73238311c829fd4d6c8
Siebe Baree <siebe.baree@outlook.com>
2023-01-03 13:54:39 +0100

    Removed old v1 code from file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,8 @@
 import { Router } from "express";
 import discordRoutes from "./discord";
-import stripeRoutes from "./stripe";
-import storeRoutes from "./store";
 
 const router = Router();
 
 router.use("/discord", discordRoutes);
-router.use("/stripe", stripeRoutes);
-router.use("/store", storeRoutes);
 
 export default router;
