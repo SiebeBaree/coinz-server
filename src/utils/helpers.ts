811c155f2e4c9c155fd5f2eca901d23284ed34b4
Siebe Baree <siebe.baree@outlook.com>
2023-01-08 18:51:28 +0100

    Removed old Discord OAuth2 code

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,16 +1,6 @@
-import { AxiosRequestConfig } from "axios";
 import { NextFunction, Request, Response } from "express";
 
-export const convertObjectToURLSearchParams = (data: any) => {
-    return new URLSearchParams(data);
-}
-
-export const userAuthHeaders = (token: string): AxiosRequestConfig => ({
-    headers: {
-        Authorization: `Bearer ${token}`,
-    }
-})
-
+// eslint-disable-next-line @typescript-eslint/ban-types
 export function runAsync(callback: Function) {
     return (req: Request, res: Response, next: NextFunction) => {
         callback(req, res, next).catch(next);
