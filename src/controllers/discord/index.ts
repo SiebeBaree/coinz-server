ba1bdf84de0191a7b6c9da9fdbbc000aa0f64086
Siebe Baree <siebe.baree@outlook.com>
2023-02-10 12:57:25 +0100

    Added controllers

--- src/controllers/discord/index.ts
+++ src/controllers/discord/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { IWebUser } from "../../models/WebUser";
-import { getUserService } from "../../services/discord";
+import { getUserService, getMemberService, getPremiumService } from "../../services/discord";
 
 export const getUserController = async (req: Request, res: Response) => {
     const user = req.user as IWebUser;
@@ -11,6 +11,36 @@ export const getUserController = async (req: Request, res: Response) => {
 
     try {
         const { data } = await getUserService(user.id);
+        res.send(data);
+    } catch (error) {
+        res.status(401).send({ error: "Unauthorized" });
+    }
+};
+
+export const getMemberController = async (req: Request, res: Response) => {
+    const user = req.user as IWebUser;
+    if (user === undefined) {
+        res.status(401).send({ error: "Unauthorized" });
+        return;
+    }
+
+    try {
+        const data = await getMemberService(user.discordId);
+        res.send(data);
+    } catch (error) {
+        res.status(401).send({ error: "Unauthorized" });
+    }
+};
+
+export const getPremiumController = async (req: Request, res: Response) => {
+    const user = req.user as IWebUser;
+    if (user === undefined) {
+        res.status(401).send({ error: "Unauthorized" });
+        return;
+    }
+
+    try {
+        const data = await getPremiumService(user.discordId);
         res.send(data);
     } catch (error) {
         res.status(401).send({ error: "Unauthorized" });
