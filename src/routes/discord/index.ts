da6f056bf00afd107238c8c92567e11f2a53ae20
Siebe Baree <siebe.baree@outlook.com>
2023-02-10 12:57:39 +0100

    Added member and premium routes

--- src/routes/discord/index.ts
+++ src/routes/discord/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getUserController } from "../../controllers/discord";
+import { getMemberController, getUserController, getPremiumController } from "../../controllers/discord";
 import { isAuthenticated } from "../../utils/middlewares";
 
 const router = Router();
 
 router.get("/user", isAuthenticated, getUserController);
+router.get("/member", isAuthenticated, getMemberController);
+router.get("/premium", isAuthenticated, getPremiumController);
 
 export default router;
