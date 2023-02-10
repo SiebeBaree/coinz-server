6bc9ac82c4a190238bb21e5c0fda8b7c7c895998
Siebe Baree <siebe.baree@outlook.com>
2023-02-10 12:57:32 +0100

    Added stripe routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth";
 import discordRoutes from "./discord";
+import stripeRoutes from "./stripe";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/discord", discordRoutes);
+router.use("/stripe", stripeRoutes);
 
 export default router;
