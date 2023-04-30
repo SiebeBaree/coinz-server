31e2e3657372aa3d161f9dad2062978a27daf973
Siebe Baree <siebe.baree@outlook.com>
2023-04-30 21:52:04 +0200

    Removed old code

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,9 @@
 import express from "express";
 import cors from "cors";
-import routes from "./routes";
 
 const app = express();
 
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", routes);
 
 export default app;
